29b3edf513ae9e6658e405d01b4481204aa4dbe1
Lusignan <mat.bures@gmail.com>
2023-05-08 12:12:58 +0200

    Table prepared

---
+++ src/pages/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Home () {
+    return(
+        <div className='container'>
+            <div className='py-4'>
+                   <table className="table border shadow">
+                   <thead>
+                           <tr>
+                             <th>Id</th>
+                             <th>Short Name</th>
+                             <th>Valid From</th>
+                             <th>Name</th>
+                             <th>Country</th>
+                             <th>Move</th>
+                             <th>Amount</th>
+                             <th>Val Buy</th>
+                             <th>Val Sell</th>
+                             <th>Val Mid</th>
+                             <th>Curr Buy</th>
+                             <th>Curr Sell</th>
+                             <th>Curr Mid</th>
+                             <th>Version</th>
+                             <th>CNB Mid</th>
+                             <th>ECB Mid</th>
+                           </tr>
+                         </thead>
+
+                   </table>
+
+                </div>
+            </div>
+        )
+   }
