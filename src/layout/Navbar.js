6936b18adb467b3016320cc1e0b743f297ed593d
Lusignan <mat.bures@gmail.com>
2023-05-08 13:49:30 +0200

    Api working with buttons

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 
 export default function Navbar() {
+  const viewExchangeRates = async () => {
+    const result = await axios.get('http://localhost:8080/api/exchangerates?usedb=true');
+    console.log(result);
+    // You can set the exchangeRates state here with the data from the API
+  };
+
+  const updateExchangeRates = async () => {
+    const result = await axios.get('http://localhost:8080/api/exchangerates?usedb=false');
+    console.log(result);
+  };
+
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
+          <a className="navbar-brand" href="home">
             Exchange Rates
           </a>
           <button
@@ -20,10 +32,12 @@ export default function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="d-flex">
-            <button className="btn btn-outline-light me-2">
+            <button className="btn btn-outline-light me-2" onClick={viewExchangeRates}>
               View Exchange Rates
             </button>
-            <button className="btn btn-outline-light">Update Exchange Rates</button>
+            <button className="btn btn-outline-light" onClick={updateExchangeRates}>
+              Update Exchange Rates
+            </button>
           </div>
         </div>
       </nav>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+ import axios from 'axios';
 
-export default function Home () {
-    return(
-        <div className='container'>
-            <div className='py-4'>
-                   <table className="table border shadow">
-                   <thead>
-                           <tr>
-                             <th>Id</th>
-                             <th>Short Name</th>
-                             <th>Valid From</th>
-                             <th>Name</th>
-                             <th>Country</th>
-                             <th>Move</th>
-                             <th>Amount</th>
-                             <th>Val Buy</th>
-                             <th>Val Sell</th>
-                             <th>Val Mid</th>
-                             <th>Curr Buy</th>
-                             <th>Curr Sell</th>
-                             <th>Curr Mid</th>
-                             <th>Version</th>
-                             <th>CNB Mid</th>
-                             <th>ECB Mid</th>
-                           </tr>
-                         </thead>
+ export default function Home() {
+   const [exchangeRates, setExchangeRates] = useState([]);
 
-                   </table>
+   useEffect(() => {
+     viewExchangeRates();
+   }, []);
 
-                </div>
-            </div>
-        )
-   }
+   const viewExchangeRates = async () => {
+     const result = await axios.get('http://localhost:8080/api/exchangerates?usedb=false');
+     setExchangeRates(result.data);
+   };
+
+   return (
+     <div className="container">
+       <div className="py-4">
+         <table className="table border shadow">
+           <thead>
+             <tr>
+               <th>Id</th>
+               <th>Short Name</th>
+               <th>Valid From</th>
+               <th>Name</th>
+               <th>Country</th>
+               <th>Move</th>
+               <th>Amount</th>
+               <th>Val Buy</th>
+               <th>Val Sell</th>
+               <th>Val Mid</th>
+               <th>Curr Buy</th>
+               <th>Curr Sell</th>
+               <th>Curr Mid</th>
+               <th>Version</th>
+               <th>CNB Mid</th>
+               <th>ECB Mid</th>
+             </tr>
+           </thead>
+
+         </table>
+       </div>
+     </div>
+   );
+ }
